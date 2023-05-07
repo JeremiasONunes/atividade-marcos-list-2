4879242c160cbe48b7585c0c85884166b21b4b69
jereh6292 <jeremias.6292@gmail.com>
2023-05-06 22:18:14 -0300

    Create Book.java
    add

---
+++ src/exercicio7/Book.java
@@ -0,0 +1,35 @@
+package exercicio7;
+
+/*Crie uma classe chamada Livro que tenha os atributos titulo, autor e ano. 
+ * Crie também um método para imprimir as informações do livro.
+ */
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public void printBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Year: " + year);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
